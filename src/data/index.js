0d9e12b0e43a4ed2c7369b147f1e9dd9e0b6ab49
samirahmad5445 <samirahmad5445@gmail.com>
2023-10-12 01:00:47 +0300

    add next three sections

--- src/data/index.js
+++ src/data/index.js
@@ -2,18 +2,78 @@
 export const features = [
   {
     name: "User Experience",
-    icon: "pencil"
+    icon: "pencil",
   },
   {
     name: "Responsive Layout",
-    icon: "tablet"
+    icon: "tablet",
   },
   {
     name: "Responsive Layout",
-    icon: "images"
+    icon: "images",
   },
   {
     name: "Support 24/7",
-    icon: "paperclip"
+    icon: "paperclip",
   },
-]
+];
+
+// for works section
+export const productImages = [
+  "project-card-1.jpg",
+  "project-card-2.jpg",
+  "project-card-3.jpg",
+  "project-card-4.jpg",
+  "project-card-5.jpg",
+  "project-card-6.jpg",
+  "project-card-7.jpg",
+  "project-card-8.jpg",
+  "project-card-9.jpg",
+];
+
+// for main services section
+export const mainServices = [
+  {
+    name: "User Experience",
+    icon: "pencil",
+  },
+  {
+    name: "Responsive Layout",
+    icon: "tablet",
+  },
+  {
+    name: "Digital Solution",
+    icon: "images",
+  },
+  {
+    name: "Bootstrap 3x",
+    icon: "desktop",
+  },
+  {
+    name: "Live Page Builder",
+    icon: "layers-linear",
+  },
+  {
+    name: "Pixel Perfect",
+    icon: "idea",
+  },
+];
+
+// for the blog section
+export const blogPosts = [
+  {
+    thumbnail: "post-01.jpg",
+    title: "How These Different Book Covers Reflect the Design",
+    date: "january 15, 2018",
+  },
+  {
+    thumbnail: "post-02.jpg",
+    title: "8 Colorful Toys Designed to Spark the Imagination",
+    date: "April 19, 2014",
+  },
+  {
+    thumbnail: "post-03.jpg",
+    title: "User Esperience Dedign is one of the most repidly developing",
+    date: "Febuary 13, 2018",
+  },
+];
