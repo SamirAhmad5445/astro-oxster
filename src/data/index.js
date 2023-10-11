d2a213b57574a2c8526670811336dfeadc9f02f0
samirahmad5445 <samirahmad5445@gmail.com>
2023-10-11 18:38:33 +0300

    init commit

---
+++ src/data/index.js
@@ -0,0 +1,19 @@
+// for the about section
+export const features = [
+  {
+    name: "User Experience",
+    icon: "pencil"
+  },
+  {
+    name: "Responsive Layout",
+    icon: "tablet"
+  },
+  {
+    name: "Responsive Layout",
+    icon: "images"
+  },
+  {
+    name: "Support 24/7",
+    icon: "paperclip"
+  },
+]
